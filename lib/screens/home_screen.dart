dda775013c3d811493024d2083c1bfbc3cd03055
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-12-03 17:00:42 +0900

    feat: thumbnail 추가

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -29,7 +29,16 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return makeList(snapshot);
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(
+                  child: makeList(snapshot),
+                )
+              ],
+            );
           }
           return const Center(
             child: CircularProgressIndicator(),
@@ -46,10 +55,21 @@ class HomeScreen extends StatelessWidget {
       itemBuilder: (context, index) {
         print(index);
         var webtoon = snapshot.data![index];
-        return Text(webtoon.title);
+        return Column(
+          children: [
+            Image.network(
+              webtoon.thumb,
+              headers: const {
+                "User-Agent":
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+              },
+            ),
+            Text(webtoon.title),
+          ],
+        );
       },
       separatorBuilder: (context, index) => const SizedBox(
-        width: 20,
+        width: 40,
       ),
     );
   }
