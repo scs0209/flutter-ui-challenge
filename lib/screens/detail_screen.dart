0a70bf517a920957a7b0a0a34cfac6976134a35d
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-12-04 13:21:33 +0900

    feat: toonflix 완료 아직 인터넷 연결 에러 못고침

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:toonflix/models/webtoon_detail_model.dart';
 import 'package:toonflix/models/webtoon_episode.dart';
 import 'package:toonflix/services/api_service.dart';
@@ -27,12 +28,44 @@ class _DetailScreenState extends State<DetailScreen> {
   // 또한 initState는 StatelessWidget에서는 사용하지 않는다. 오직 StatefulWidget에서만 사용한다.
   late Future<WebtoonDetailModel> webtoon;
   late Future<List<WebtoonEpisodeModel>> episodes;
+  late SharedPreferences prefs;
+  bool isLiked = false;
+
+  Future initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    final likedToons = prefs.getStringList('likedToons');
+    if (likedToons != null) {
+      if (likedToons.contains(widget.id) == true) {
+        setState(() {
+          isLiked = true;
+        });
+      }
+    } else {
+      await prefs.setStringList("likedToons", []);
+    }
+  }
 
   @override
   void initState() {
     super.initState();
     webtoon = AppService.getToonById(widget.id);
     episodes = AppService.getLatestEpisodesById(widget.id);
+    initPrefs();
+  }
+
+  onHeartTap() async {
+    final likedToons = prefs.getStringList('likedToons');
+    if (likedToons != null) {
+      if (isLiked) {
+        likedToons.remove(widget.id);
+      } else {
+        likedToons.add(widget.id);
+      }
+      await prefs.setStringList("likedToons", likedToons);
+    }
+    setState(() {
+      isLiked = !isLiked;
+    });
   }
 
   @override
@@ -43,6 +76,14 @@ class _DetailScreenState extends State<DetailScreen> {
         elevation: 2,
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
+        actions: [
+          IconButton(
+            onPressed: onHeartTap,
+            icon: Icon(
+              isLiked ? Icons.favorite_rounded : Icons.favorite_outline,
+            ),
+          )
+        ],
         title: Center(
           child: Text(
             widget.title,
--- lib/widgets/episode_widget.dart
+++ lib/widgets/episode_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_episode.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class Episode extends StatelessWidget {
   const Episode({
@@ -13,10 +13,8 @@ class Episode extends StatelessWidget {
   final WebtoonEpisodeModel episode;
 
   onButtonTap() async {
-    final url = Uri.parse("https://google.com");
-    await launchUrl(url);
-    // await launchUrlString(
-    //     "https://comic.naver.com/webtoon/detail?titleId=$webtoonId&no=${episode.id}");
+    await launchUrlString(
+        "https://comic.naver.com/webtoon/detail?titleId=$webtoonId&no=${episode.id}");
   }
 
   @override
